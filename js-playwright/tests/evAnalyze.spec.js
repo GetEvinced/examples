31e94123c08705f5dda81056bc42582db5b33ec9
Shane C <shane@evinced.com>
2024-12-06 22:46:14 +0000

    playwright baby

---
+++ js-playwright/tests/evAnalyze.spec.js
@@ -0,0 +1,14 @@
+import { test, expect } from "@playwright/test";
+import { existsSync } from "node:fs";
+import { EvincedSDK } from "@evinced/js-playwright-sdk";
+
+test.describe("Evinced evAnalyze", () => {
+  test("Single test run using evAnalyze", async ({ page }) => {
+    const evReport = "./test-results/evAnalyze.html";
+    const evincedService = new EvincedSDK(page);
+    await page.goto("https://demo.evinced.com/");
+    const issues = await evincedService.evAnalyze();
+    await evincedService.evSaveFile(issues, "html", evReport);
+    expect(existsSync(evReport)).toBeTruthy();
+  });
+});
