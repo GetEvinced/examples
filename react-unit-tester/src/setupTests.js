d19ce99487c28f62ed8286bdc2f70e16760c6d60
Shane C <shane@evinced.com>
2024-12-27 11:13:38 +0000

    unit tester is now initialised using the configure method in setupTests

--- react-unit-tester/src/setupTests.js
+++ react-unit-tester/src/setupTests.js
@@ -6,6 +6,13 @@ import '@testing-library/jest-dom';
 
 import * as React from 'react';
 import { act } from 'react-dom/test-utils';
+import { configure } from "@evinced/unit-tester";
+
+configure({
+    serviceAccountId: process.env.EVINCED_SERVICE_ID,
+    serviceAccountSecret:
+        process.env.EVINCED_API_KEY,
+});
 
 /* eslint-disable no-undef */
 globalThis.act = React.act || act;
