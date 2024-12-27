d85f01fe43543df9d9c28c401c19468465dd7d3f
Shane C <shane@evinced.com>
2024-12-27 12:25:13 +0000

    added multiple tests for UT

--- react-unit-tester/src/components/Button.test.js
+++ react-unit-tester/src/components/Button.test.js
@@ -2,6 +2,7 @@ import React from "react";
 import { render, screen, fireEvent } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import Button from "./Button";
+import EvincedUT from "@evinced/unit-tester";
 
 describe("Button Component", () => {
   test("renders the button with correct text and class", () => {
@@ -20,5 +21,12 @@ describe("Button Component", () => {
     const button = screen.getByRole("button", { name: /open modal/i });
     fireEvent.click(button);
     expect(openModalMock).toHaveBeenCalledTimes(1);
+  });
+
+  test("Ev Analyze Button", async () => {
+    render(<Button />);
+
+    const results = await EvincedUT.analyzeButton({ role: "button" });
+    expect(results).toHaveNoFailures();
   });
 });
--- react-unit-tester/src/setupTests.js
+++ react-unit-tester/src/setupTests.js
@@ -2,16 +2,15 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+import "@testing-library/jest-dom";
 
-import * as React from 'react';
-import { act } from 'react-dom/test-utils';
+import * as React from "react";
+import { act } from "react-dom/test-utils";
 import { configure } from "@evinced/unit-tester";
 
 configure({
-    serviceAccountId: process.env.EVINCED_SERVICE_ID,
-    serviceAccountSecret:
-        process.env.EVINCED_API_KEY,
+  serviceAccountId: process.env.EVINCED_SERVICE_ID,
+  serviceAccountSecret: process.env.EVINCED_API_KEY,
 });
 
 /* eslint-disable no-undef */
