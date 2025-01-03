f8bbade7d81e9f129c015d0ad2c32fc6c9fd26a2
Shane C <shane@evinced.com>
2025-01-03 13:10:01 +0000

    added browserVersion: stable to use a stable version of chrome on wdio, running locally now

--- js-wdio-web/wdio.conf.js
+++ js-wdio-web/wdio.conf.js
@@ -25,6 +25,7 @@ export const config = {
   capabilities: [
     {
       browserName: "chrome",
+      browserVersion: "stable",
       "goog:chromeOptions": {
         args: [
           "--headless",
