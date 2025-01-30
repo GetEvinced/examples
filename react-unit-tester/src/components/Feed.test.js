3909dd8296ee92903e12828b3bc98536b2e8a02a
Shane C <shane@evinced.com>
2025-01-30 10:52:42 +0000

    remove import

--- react-unit-tester/src/components/Feed.test.js
+++ react-unit-tester/src/components/Feed.test.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { render, waitFor, screen } from "@testing-library/react";
-import "@testing-library/jest-dom/extend-expect";
 import Feed from "./Feed";
 import EvincedUT from "@evinced/unit-tester";
 
