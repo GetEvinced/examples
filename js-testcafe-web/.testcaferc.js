27595e4307e670acf136525b300a185b0d37cefa
Kris-Evinced <kristen.sutherland@evinced.com>
2025-03-21 16:54:19 -0700

    adding js-testcafe examples/.testcaferc/evConfig for web sdk

---
+++ js-testcafe-web/.testcaferc.js
@@ -0,0 +1,54 @@
+const { setCredentials, EvincedSDK } = require("@evinced/js-testcafe-sdk");
+
+module.exports = {
+  hooks: {
+    // Each unique launch of TestCafe constitutes a single test run.
+    // Test run hooks cannot access the browser.
+    testRun: {
+      before: async () => {
+        console.log('Global testRun before hook');
+      },
+      after: async () => {
+        console.log('Global testRun after hook');
+      },
+      // Global fixture hooks run before/after each of the fixtures in your test suite.
+      // Fixture hooks cannot access the browser.
+    },
+    fixture: {
+      before: async () => {
+        // Set credentials
+        try {
+          console.log("Accessing Global Credentials");
+
+          await setCredentials({
+            serviceId: process.env.EVINCED_SERVICE_ID,
+            secret: process.env.EVINCED_API_KEY,
+          });
+          // To configure Offline Credentials use below example
+          // await setOfflineCredentials({
+          //   serviceId: process.env.EVINCED_SERVICE_ID,
+          //   token: process.env.EVINCED_AUTH_TOKEN,
+          // });
+        } catch (error) {
+          console.log("Error Evinced SDK authorization failure ", error);
+        }
+      },
+      after: async () => {
+        console.log("Global Fixture after hook");
+      },
+    },
+  },
+  // Global test hooks run before/after each of the tests in your entire test suite.
+  // Hooks that run before and after tests can access the browser.
+  test: {
+    // before: async t => {
+    //   console.log('Global Test before hook');
+    //   t.ctx.evinced = new EvincedSDK(t);
+    //   await t.ctx.evinced.evStart();
+    // },
+    // after: async t => {
+    //   console.log('Global Test after hook');
+    //   await t.ctx.evinced.evStop();
+    // },
+  },
+};
