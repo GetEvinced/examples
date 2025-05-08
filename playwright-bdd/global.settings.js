7fbdd3466937518b54fd5089fe4f30974b697fd2
Shane C <shane@evinced.com>
2025-05-08 13:47:39 +0800

    show how to use offlineCredentials as well

--- playwright-bdd/global.settings.js
+++ playwright-bdd/global.settings.js
@@ -1,4 +1,7 @@
-import { setCredentials } from "@evinced/js-playwright-sdk";
+import {
+  setCredentials,
+  setOfflineCredentials,
+} from "@evinced/js-playwright-sdk";
 
 export default async function globalSetup(config) {
   try {
@@ -6,6 +9,11 @@ export default async function globalSetup(config) {
       serviceId: process.env.EVINCED_SERVICE_ID,
       secret: process.env.EVINCED_API_KEY,
     });
+
+    // await setOfflineCredentials({
+    //   serviceId: process.env.EVINCED_SERVICE_ID,
+    //   token: process.env.EVINCED_WEB_OFFLINE_TOKEN,
+    // });
   } catch (error) {
     throw new Error("Evinced SDK authorization failure.");
   }
