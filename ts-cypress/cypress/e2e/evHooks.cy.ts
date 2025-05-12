144063116b6dcd87b27706bd9a244c8d4fc7ffee
Shane C <shane@evinced.com>
2025-05-12 11:33:28 +0800

    les try again

--- ts-cypress/cypress/e2e/evHooks.cy.ts
+++ ts-cypress/cypress/e2e/evHooks.cy.ts
@@ -25,8 +25,8 @@ context("Evinced Demo Site tests", () => {
     const TINY_HOME_OPTION = `${BASE_FORM_SELECTOR} > div:nth-child(1) > div > ul > li:nth-child(2)`;
     const EAST_COST_OPTION = `${BASE_FORM_SELECTOR} > div:nth-child(2) > div > ul > li:nth-child(3)`;
 
-    cy.get(SELECT_HOME_DROPDOWN).click({ force: true });
-    cy.get(TINY_HOME_OPTION).should("be.visible").click();
+    cy.get(SELECT_HOME_DROPDOWN).click();
+    cy.get(TINY_HOME_OPTION).scrollIntoView().should("be.visible").click();
 
     cy.get(SELECT_WHERE_DROPDOWN).click();
     cy.get(EAST_COST_OPTION).should("be.visible").click(); 
