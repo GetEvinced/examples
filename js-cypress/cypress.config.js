9ae252a08b9c7199d50fe44a128ada67bab23447
Shane C <shane@evinced.com>
2025-07-17 16:16:14 +0100

    up to date example

--- js-cypress/cypress.config.js
+++ js-cypress/cypress.config.js
@@ -15,6 +15,7 @@ module.exports = defineConfig({
     serviceId: process.env.EVINCED_SERVICE_ID,
     secret: process.env.EVINCED_API_KEY,
     evincedConfig: {
+      switchOn: false,
       reporterOptions: {
         reportFormat: "html", 
         filePath: "./reports/aggregatedReport.html", 
