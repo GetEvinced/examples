14986948db037dbb2de32a331e688b9eabe3ee8a
Shane C <shane@evinced.com>
2025-09-03 16:35:25 +0100

    adjust config

--- js-cypress/cypress.config.js
+++ js-cypress/cypress.config.js
@@ -7,7 +7,7 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
       on("task", {
-        uploadToPlatform: Evinced.cyTaskUploadToPlatform,
+        evTask: Evinced.cyEvTask
       });
     },
   },
