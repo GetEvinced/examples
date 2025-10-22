93015b332808c9af2e17fa5f9f2b3c27355b5feb
Kris Sutherland <kristen.sutherland@evinced.com>
2025-10-22 08:46:16 -0700

    tidying up java-selenium-sdk examples and adding sauce labs example (#178)
    * tidying up java-selenium-sdk examples and adding sauce labs example
    
    * adding java selenium workflow
    
    ---------
    
    Co-authored-by: Kris-Evinced <“kristen.sutherland@evinced.com”>
    Co-authored-by: Shane C <shane@evinced.com>

--- java-selenium-web/src/test/java/com/examples/EvincedSetupExample.java
+++ java-selenium-web/src/test/java/com/examples/EvincedSetupExample.java
@@ -1,9 +1,9 @@
-package com.krissutherland;
+package com.examples;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import com.evinced.common.impl.configuration.LoggingConfiguration;
-import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import com.evinced.dto.configuration.EvincedConfiguration;
@@ -17,24 +17,49 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.io.File;
 import java.net.MalformedURLException;
-import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
-public class EvincedSetupTest
+
+public class EvincedSetupExample
 {
     ChromeDriver driver;
     EvincedWebDriver evincedDriver;
+    EvincedConfiguration evincedConfiguration;
 
     @Before
-    public void setupAll() {
+    public void setup() {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
+
+        // Add time for unique log folder per run
+        LocalDate date = LocalDate.now();
+        // Creating log folder
+        String targetLogsFolder = "target/logs/evinced/debug_logs_" + date.format(DateTimeFormatter.ISO_DATE);
+        
+
+        // Evinced configuration
+        evincedConfiguration = new EvincedConfiguration();
+        evincedConfiguration.setEnableScreenshots(true);
+
+        // Set offline credentials from environment variables
         EvincedSDK.setOfflineCredentials(System.getenv("EVINCED_SERVICE_ID"), System.getenv("EVINCED_AUTH_TOKEN"));
+
+        // Evinced logging configuration
+        evincedConfiguration.setLoggingConfiguration(LoggingConfiguration.builder()
+            .loggingEnabled(false)
+            .loggingLevel("debug") // set to trace for most verbose output
+            .logsFolder(targetLogsFolder)
+            .systemInformationLoggingEnabled(true)
+            .build());
+        
+        // Passing in the Evinced Configuration
+        evincedDriver = new EvincedWebDriver(driver, evincedConfiguration);
     }
 
     @After
-    public void teardownDrivers() {
+    public void teardown() {
         if (driver != null) {
             driver.quit();
         }
@@ -45,42 +70,29 @@ public class EvincedSetupTest
 
     // test for https://developer.evinced.com/sdks-for-web-apps/selenium-java-sdk#addevincedaccessibilitychecks(singlerunmode)
     @Test
-    public void ShouldStartChrome() throws MalformedURLException {
-       
-        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss Z");
-        String targetLogsFolder = "target/logs/evinced/debug_logs_" + dateFormat.format(new java.util.Date());
-  
-        EvincedConfiguration evincedConfiguration = new EvincedConfiguration();
-        evincedConfiguration.setLoggingConfiguration(LoggingConfiguration.builder()
-            .loggingEnabled(false)
-            .loggingLevel("debug")
-            .logsFolder(targetLogsFolder)
-            .systemInformationLoggingEnabled(true)
-            .build());
-        EvincedWebDriver evincedDriver = new EvincedWebDriver(driver, evincedConfiguration);
-
+    public void shouldStartChrome() throws MalformedURLException {
+        // go to Evinced demo page
         evincedDriver.get("https://demo.evinced.com");
+        
+        // Analyze the page for accessibility issues and return a report
         Report report = evincedDriver.evAnalyze();
-        // Assert that there are SOME accessibility issues
+        
+        // Assert that there are accessibility issues
         assertTrue(report.getIssues().size() != 0);
     }
 
     @Test
-    public void SaveReports() throws MalformedURLException {
+    public void saveReports() throws MalformedURLException {
         try {
-            EvincedConfiguration configuration = new EvincedConfiguration();
-            configuration.setEnableScreenshots(true);
-            EvincedWebDriver evincedDriver = new EvincedWebDriver(driver, configuration);
-            EvincedSDK.setOfflineCredentials(System.getenv("EVINCED_SERVICE_ID"), System.getenv("EVINCED_AUTH_TOKEN"));
-            
+            // go to Evinced demo page
             evincedDriver.get("https://demo.evinced.com");
             
-            Report landing_page_report = evincedDriver.evAnalyze();
+            Report landingPageReport = evincedDriver.evAnalyze();
             
             //Saves issues in a file with the specified format and location. 
             //Supported formats - json, html, sarif, and csv.
-            EvincedReporter.evSaveFile("landing_page",landing_page_report, EvincedReporter.FileFormat.HTML);
-            assertTrue(landing_page_report.getIssues().size() != 0);
+            EvincedReporter.evSaveFile("landing_page",landingPageReport, EvincedReporter.FileFormat.HTML_v2_1);
+            assertFalse(landingPageReport.getIssues().isEmpty());
 
             evincedDriver.get("https://demo.evinced.com/results/?what=Tiny%20House&where=Canada&date=Tue%20Jul%2009%202024%2011:21:39%20GMT-0400%20(Eastern%20Daylight%20Time");
             takeSnapshot("results_page", evincedDriver);
@@ -91,17 +103,14 @@ public class EvincedSetupTest
         }
     }
 
-    public static void takeSnapshot(String reportName, EvincedWebDriver evDriver) {
+    public static void takeSnapshot(String reportName, EvincedWebDriver evincedDriver) {
         /* use your local driver class instead and configure it 
          here if you need to call evAnalyze sporadically */
-        // EvincedConfiguration evConfiguration = new EvincedConfiguration();
-        // evConfiguration.setEnableScreenshots(true);
-        // EvincedWebDriver evincedDriver = new EvincedWebDriver(localDriverInstance, evConfiguration);
         if (Boolean.getBoolean("RUN_EVINCED") != true) {
             return;
         }
-        Report report = evDriver.evAnalyze();
-        EvincedReporter.evSaveFile(reportName, report, EvincedReporter.FileFormat.HTML);
+        Report report = evincedDriver.evAnalyze();
+        EvincedReporter.evSaveFile(reportName, report, EvincedReporter.FileFormat.HTML_v2_1);
     }
 
 }
